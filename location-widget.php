5286188c004befbfeddf557355dcbc048783f75f
Tripti Sharma <tripti.sharma@bellevuecollege.edu>
2014-06-23 11:28:06 -0700

    Updated version number to 1.0.0.1 for testing

--- location-widget.php
+++ location-widget.php
@@ -3,7 +3,7 @@
 Plugin Name:  Bellevue College Location Widget
 Plugin URI:   https://github.com/BellevueCollege/location-widget/
 Description:  Department/Unit Location Widget.
-Version:      1
+Version:      1.0.0.1
 Author:       Bellevue College DevCom
 Author URI:   http://www.bellevuecollege.edu/
 */
